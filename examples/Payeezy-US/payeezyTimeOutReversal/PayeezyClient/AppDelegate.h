e3643207bee96739246fa6e4b00b77d7f9909a9c
Atul Parmar <Atul.Parmar@firstdata.com>
2015-10-29 18:38:15 -0400

    Payeezy Oct 30, 2015 Release
    Payeezy Oct 30, 2015 Release
    Timeout Reversal

---
+++ examples/Payeezy-US/payeezyTimeOutReversal/PayeezyClient/AppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.h
+//  PayeezyClient
+//
+//  Created by First Data Corporation on 5/28/2015.
+//  Copyright (c) 2015 First Data Corporation. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
+
